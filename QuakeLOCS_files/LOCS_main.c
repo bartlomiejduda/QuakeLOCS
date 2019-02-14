4635a1c9790b6bf88cd22a8df604518ad5802202
bartlomiejduda <nomail@example.com>
2019-02-14 16:37:59 +0100

    First build

---
+++ QuakeLOCS_files/LOCS_main.c
@@ -0,0 +1,14 @@
+#include "LOCS_main.h"
+#include "quakedef.h"
+
+
+// V1.00 14.02.19 Bartlomiej Duda
+
+void get_q_text(char *q_identifier, char *q_lang)
+{
+	if (q_lang == "")
+	{
+		q_lang = "ENG";
+	}
+
+}
