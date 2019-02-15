ab7e302634c09b238a4893c0994604507a3168ae
bartlomiejduda <nomail@example.com>
2019-02-15 13:12:35 +0100

    minor changes

--- QuakeLOCS_files/LOCS_main.c
+++ QuakeLOCS_files/LOCS_main.c
@@ -1,11 +1,29 @@
 #include "LOCS_main.h"
 #include "quakedef.h"
+#include <stdio.h>
 
 
 // V1.00 14.02.19 Bartlomiej Duda
 
-void get_q_text(char *q_identifier, char *q_lang)
+struct config_global 
 {
+	char  lang[50];
+	char  lang_code[10];
+};
+
+
+void get_q_global_config(FILE *fp)
+{
+	fp = fopen(".\\LOCS\\locs_config.txt", "rt");
+
+	fclose(fp);
+}
+
+void get_q_text(char *q_identifier)
+{
+	char *q_lang = "";
+	// q_lang = get_q_lang();   <--TODO
+
 	if (q_lang == "")
 	{
 		q_lang = "ENG";
