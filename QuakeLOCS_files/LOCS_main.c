fbd4920635bff2f6767f73dd139ae96e4c2012ee
Bartlomiej Duda <ikskoks@gmail.com>
2019-04-26 13:56:30 +0200

    Minor changes

--- QuakeLOCS_files/LOCS_main.c
+++ QuakeLOCS_files/LOCS_main.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 #include "..\Quake\quakedef.h"
 
 // V1.00 14.02.19 Bartlomiej Duda
+// V1.01 19.03.19 Bartlomiej Duda
 
 struct lang_line
 {
@@ -23,29 +25,6 @@ int lang_counter = 0;
 
 
 
-/*
-static int q_global_handler(void* user, const char* section, const char* name,
-	const char* value)
-{
-
-#define MATCH(s, n) strcmp(section, s) == 0 && strcmp(name, n) == 0
-
-	if (MATCH("Available langs", name)) 
-	{
-		*line_arr[lang_counter].lang_code = *name;
-		*line_arr[lang_counter].lang = *value;
-		lang_counter++;
-
-	}
-
-	else {
-		return 0;  /* unknown section/name, error 
-	}
-	return 1;
-}
-*/
-
-
 int starts_with(const char *a, const char *b)
 {
 	if (strncmp(a, b, strlen(b)) == 0) return 1;
@@ -62,12 +41,79 @@ char* get_substring(const char* input, int offset, int len)
 	}
 
 	 char *new = malloc(sizeof(char)*len + 1);
-	//*dest = malloc(sizeof(char)*len + 1);
-
 	strncpy(new, input + offset, len);
 	new[len] = '\0';
 	return new;
 }
+
+
+char* str_to_upper(char *input)
+{
+	int input_len = strlen(input);
+	char *new = malloc(sizeof(char)*input_len);
+	int i = 0;
+
+	while (*input) 
+	{
+		*(new+i) = toupper((unsigned char)*input);
+		input++;
+		i++;
+
+	}
+	new[input_len] = '\0';
+
+	return new;
+}
+
+
+char* str_split_and_get_element(char* a_str, char* a_delimeter, int a_position)
+{
+	char* result_ptr;
+	int counter = 0;
+	int a_str_len = strlen(a_str);
+
+	if (a_position <= 0)
+		return "error_split_010";
+
+	char* temp_str = malloc(a_str_len * sizeof(*temp_str));
+	strcpy(temp_str, a_str);
+
+	char *ptr = strtok(temp_str, "="); //get first element from string
+
+	while (ptr != NULL) //iterating through splitted elements
+	{
+		if (a_position == 1)
+		{
+			result_ptr = ptr; //get first element from splitted string
+			break;
+		}
+
+
+		counter++;
+		ptr = strtok(NULL, "=");
+		result_ptr = ptr;
+
+		if (counter >= a_position - 1)
+		{
+			break;
+		}
+
+
+	}
+
+	if (ptr == NULL && result_ptr == NULL)
+		return "error_split_020"; //a_position is larger than number of splitted elements
+								  //this error also happens if string cannot be splitted (wrong delimeter for example)
+
+	int res_str_len = strlen(result_ptr);
+	char* result = malloc(res_str_len * sizeof(*result));
+	strcpy(result, result_ptr);
+	//free(temp_str);
+	return result;
+}
+
+
+
 
 
 int get_q_global_config()
@@ -75,39 +121,71 @@ int get_q_global_config()
 	Con_Printf("Starting get_q_global_config... \n");
 	char *path = ".\\LOCS\\locs_config.txt";
 	FILE *glob_config_file = fopen(path, "rt");
-	//char * line = NULL;
 	int len = 0;
-	//ssize_t read;
-	char *current_section;
+	char *current_section = "";
 	int line_counter = 0;
-
-	//struct lang_line s_line;
-
-
 	char line[500]; 
+	int lang_counter = 0;
+
+	int count_tok1 = 0;
+	int count_tok2 = 0;
+	char* current_lang;
+	char* current_lang_code;
+
+
 	while (fgets(line, sizeof line, glob_config_file) != NULL) 
 	{
 		line_counter++;
 		Con_Printf("Getting line %d: %s \n", line_counter, line);
 		if (strstr(line, "[") != NULL && strstr(line, "]") != NULL)
 		{
-			//Con_Printf("Getting [[[[[[[ \n");
 			len = strlen(line);
-			//current_section = "";
-			//get_substring(line, current_section, 1, len - 1);
-			//printf("Current section: %s", current_section);
 
-			//current_section = strtok(line, "["); // find the first double quote
-			//current_section = strtok(NULL, "]");
-
+			line[strcspn(line, "\r\n")] = 0;
 			current_section = get_substring(line, 1, len-3);
-
-
-
 			Con_Printf("####Current section: %s \n", current_section);
-			//Con_SafePrintf(
+
+			/*char *cmp1 = str_to_upper(current_section);
+			char *cmp2 = str_to_upper("Available langs");
+
+			Con_Printf("####Cmp1: %s\n", cmp1);
+			Con_Printf("####Cmp2: %s\n", cmp2);*/
+
+
 		}
-		//printf("%s", line);
+
+		if (current_section != NULL && current_section != "")
+		{
+			if ( strcmp(str_to_upper(current_section) , str_to_upper("Available langs") ) == 0)
+			{
+				if (strstr(line, current_section) != NULL) 
+				{
+					continue;
+				}
+
+
+				struct lang_line a;
+				*a.lang_code = str_split_and_get_element(line, '=', 1);
+				*a.lang = str_split_and_get_element(line, '=', 2);
+
+				//*line_arr[lang_counter].lang = *(tokens + 0);
+				//line_arr[lang_counter].lang_code = *(tokens + 1);
+				//Con_Printf("####token1: %s, token2: %s \n", tokens[0][0], tokens[0][1]);
+
+				Con_Printf("####lang: %s, lang_code: %s \n", a.lang, a.lang_code);
+
+				
+
+
+				
+
+
+				
+			}
+		}
+
+		
+
 	}
 
 
